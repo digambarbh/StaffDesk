8db78b8b85dd04b54af67719d78021ca2205eb94
digambar <dbhujbal287@gmail.com>
2026-09-10 23:06:56 +0530

    feat : add validtor usinf zod
    - added a validator for a signin
    
    - added validator for a create user

---
+++ app/api/src/validator/auth.validator.ts
@@ -0,0 +1,16 @@
+import {email, z} from "zod"
+
+export const signInValidation=z.object({
+    email:z.string().email(),
+    password:z.string().min(8,"password is required")
+})
+
+export type signInInput=z.infer<typeof signInValidation>;
+
+export const createUserValidation=z.object({
+    email:z.string().email(),
+    password:z.string().min(1,"password is rquuired "),
+    role:z.enum(["admin","manager","employee"])
+})
+
+export type createUserInput=z.infer<typeof createUserValidation>;
